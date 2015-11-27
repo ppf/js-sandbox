060c239543eb1748e77251afc1de676867460819
Piotr Francuz <pfrancuz1@gmail.com>
2015-11-27 08:21:11 +0100

    add willy64 to boxes refactor node installing

--- Vagrantfile
+++ Vagrantfile
@@ -4,13 +4,13 @@
 Vagrant.configure(2) do |config|
 
   config.vm.provider "virtualbox" do |v|
-    v.name = "linux_vargrant2"
+    v.name = "linux_villy64"
     v.gui = false
     v.memory = 1024
     v.cpus = 1
   end
 
-  config.vm.box = "hashicorp/precise32"
+  config.vm.box = "villy64"
   config.vm.provision :shell, path: "bootstrap.sh"
   config.vm.network :forwarded_port, guest: 3000, host: 3000
 
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 
-sudo apt-get update
-sudo apt-get install -y python-software-properties python g++ make
-sudo add-apt-repository -y ppa:chris-lea/node.js
+
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
+
 sudo apt-get update
+
+sudo curl -sL https://deb.nodesource.com/setup | sudo bash -
+sudo apt-get install nodejs
+sudo apt-get install -y build-essential
+
+sudo apt-get install git vim mc
+
 sudo apt-get install -y nodejs
 sudo apt-get install -y npm
 sudo apt-get install -y mongodb-org
-sudo npm install -g bower
-sudo npm install -g mongoose
-sudo npm install -g express
-
-npm install
